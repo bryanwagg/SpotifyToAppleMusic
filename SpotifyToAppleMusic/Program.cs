9c376142d6e4336a04429f667fd5444786392eb4
Bryan Wagg <bryanwagg@icloud.com>
2019-10-19 12:31:06 +0100

    added initial functionality to get spotify tracks from playlist

--- SpotifyToAppleMusic/Program.cs
+++ SpotifyToAppleMusic/Program.cs
@@ -12,7 +12,7 @@ namespace SpotifyToAppleMusic
         const string SpotifyClientID = "";
         const string SpotifyClientSecret = "";
 
-        private static readonly HttpClient client = new HttpClient();
+        //private static readonly HttpClient client = new HttpClient();
 
 
         static void Main(string[] args)
@@ -21,6 +21,9 @@ namespace SpotifyToAppleMusic
 
             if (newToken != null)
             {
+                Console.WriteLine("Please enter the Spotify PlaylistID: ");
+                string spotiPlaylistID = Console.ReadLine();
+                GetSpotifyPlaylist(spotiPlaylistID, newToken).Wait();
                 //here we should go on to retrieve the playlist
 
                 //once thats sucessful, search each track using apple music api and get an id for best match
@@ -68,6 +71,41 @@ namespace SpotifyToAppleMusic
                 }
             }
         }
+        /* This method returns the playlist as a list of tracks.
+         * API information taken from https://developer.spotify.com/documentation/web-api/reference/playlists/get-playlists-tracks/
+         * If returns an error, its likely the playlist is private, or just outight doesnt exist.
+         */
+        private static async Task GetSpotifyPlaylist(string playlistID, SpotifyToken spotifyToken)
+        {
+
+            string url = String.Format("https://api.spotify.com/v1/playlists/{0}/tracks", playlistID);
+            Console.WriteLine("Getting Spotfiy Playlist...");
+            using (var httpClient = new HttpClient())
+            {
+                {
+                    httpClient.DefaultRequestHeaders.Clear();
+                    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(spotifyToken.Type, spotifyToken.Token);
+
+                    var response = await httpClient.GetAsync(url);
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine(
+                            "Spotify Returned an error while retrieving the playlist, the status code was: {0}. \n" +
+                            "Please ensure that you are using the Spotify Playlist ID and not the URI.\n" +
+                            "Please also ensure the playlist exists and is public.",
+                            response.StatusCode
+                        );
+                       
+                    }
+
+                    var responseMessage = await response.Content.ReadAsStringAsync();
+                    // var serializer = new DataContractJsonSerializer(typeof(SpotifyToken));
+                    Console.WriteLine(responseMessage);
+                    // return serializer.ReadObject(responseMessage) as SpotifyToken;
+                }
+            }
+        }
     }
 
 }
+;

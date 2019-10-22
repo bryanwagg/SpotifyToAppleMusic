15f6f8e2776f16b4243028a573ca82e68696bb77
Bryan Wagg <bryanwagg@icloud.com>
2019-10-22 08:25:30 +0100

    added 2 spotify calls, one retrieve playlist information, one too retrieve tracks

--- SpotifyToAppleMusic/Program.cs
+++ SpotifyToAppleMusic/Program.cs
@@ -12,7 +12,7 @@ namespace SpotifyToAppleMusic
         const string SpotifyClientID = "";
         const string SpotifyClientSecret = "";
 
-        private static readonly HttpClient client = new HttpClient();
+        //private static readonly HttpClient client = new HttpClient();
 
 
         static void Main(string[] args)
@@ -21,7 +21,37 @@ namespace SpotifyToAppleMusic
 
             if (newToken != null)
             {
-                //here we should go on to retrieve the playlist
+                Console.WriteLine(newToken.Token);
+
+                // we need to user the spotify uri here instead.
+                //TODO: split the incoming string on : and use the last one
+
+                Console.Write("Please enter the Spotify PlaylistID: ");
+                string spotiPlaylistID = Console.ReadLine();
+                var playlistInfo = GetSpotifyPlaylist(spotiPlaylistID, newToken).Result;
+                Console.WriteLine("The playlist name is {0} and has {1} songs.", playlistInfo.Name, playlistInfo.Tracks.Total);
+
+                // At this point we need to creat an array of query strings that the Apple Music Calls will loop through to search and get objects
+                while(playlistInfo.Tracks.Total > 0)
+                {
+                    if (playlistInfo.Tracks.Total < 100)
+                    {
+                        // if less than 100 tracks, we only have to make one call and only 1 object will be returned
+                        GetSpotifyPlaylistTracks(spotiPlaylistID, 0, newToken).Wait();
+                    } else
+                    {
+                        //for loop to make multiple calls, to the spotify endpoints and deals with multiple spotify paging objects.
+                        for (int i = 0; i < max; i++)
+                        {
+                            //code here
+                        }
+                    }
+                }
+
+                
+                var SpotifyTracks = GetSpotifyPlaylistTracks(spotiPlaylistID, 0, newToken).Result;
+
+                //GetSpotifyPlaylistTracks().Result();
 
                 //once thats sucessful, search each track using apple music api and get an id for best match
 
@@ -68,6 +98,73 @@ namespace SpotifyToAppleMusic
                 }
             }
         }
-    }
 
+        /* This method returns the playlist as a list of tracks.
+         * API information taken from https://developer.spotify.com/console/get-playlist/
+         * If returns an error, its likely the playlist is private, or just outight doesnt exist.
+         *
+         * we have 2 different urls because spotify is stupid and doesnt allow us to get the name when using /playist/name, only /playlist although both return tracks.
+         * https://api.spotify.com/v1/playlists/{0}/?fields=name,tracks.total should be used to first asses how many tracks we have and then use
+         * https://api.spotify.com/v1/playlists/{0}/tracks?fields=items(track(name,album(name),artists))&offset={1} to get all the tracks after that.
+         */
+        private static async Task<SpotifyPlaylistInfo> GetSpotifyPlaylist(string playlistID, SpotifyToken spotifyToken)
+        {
+            string url = String.Format("https://api.spotify.com/v1/playlists/{0}/?fields=name,tracks.total", "3C69UXWRIO8HwRzwqAGWaz");
+            Console.WriteLine("Getting Spotfiy Playlist...");
+            using (var httpClient = new HttpClient())
+            {
+                {
+                    httpClient.DefaultRequestHeaders.Clear();
+                    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(spotifyToken.Type, spotifyToken.Token);
+
+                    var response = await httpClient.GetAsync(url);
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine(
+                            "Spotify Returned an error while retrieving the playlist, the status code was: {0}. \n" +
+                            "Please ensure that you are using the Spotify URI.\n",
+                            response.StatusCode
+                        );
+
+                    }
+
+                    var responseMessage = await response.Content.ReadAsStreamAsync();
+                    var serializer = new DataContractJsonSerializer(typeof(SpotifyPlaylistInfo));
+                    
+                    return serializer.ReadObject(responseMessage) as SpotifyPlaylistInfo;
+                }
+            }
+        }
+
+        /* This method returns the playlist as a list of tracks.
+     * API information taken from https://developer.spotify.com/documentation/web-api/reference/playlists/get-playlists-tracks/
+     * If returns an error, its likely the playlist is private, or just outight doesnt exist.
+     */
+        private static async Task<SpotifyPaging> GetSpotifyPlaylistTracks(string playlistID, int offset, SpotifyToken spotifyToken)
+        {
+            string url = String.Format("https://api.spotify.com/v1/playlists/{0}/tracks?fields=items(track(name,album(name),artists))&offset={1}", playlistID, offset);
+            Console.WriteLine("Getting Spotfiy Tracks...");
+            using (var httpClient = new HttpClient())
+            {
+                {
+                    httpClient.DefaultRequestHeaders.Clear();
+                    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(spotifyToken.Type, spotifyToken.Token);
+
+                    var response = await httpClient.GetAsync(url);
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine("Something went seriously wrong, it worked 2 seconds ago. Here is the message: ");
+                        Console.WriteLine(await response.Content.ReadAsStringAsync());
+                        return null;
+                    }
+                    var responseMessage = await response.Content.ReadAsStreamAsync();
+                    var serializer = new DataContractJsonSerializer(typeof(SpotifyPaging));
+                    
+                    return serializer.ReadObject(responseMessage) as SpotifyPaging;
+                }
+            }
+        }
+    }
 }
+
+
